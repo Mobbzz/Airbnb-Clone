174a76e9b48c3df17a6119c20da1ca3412eb3699
Shad <shad_1994@hotmail.com>
2024-11-12 23:22:09 +0100

    some style fix

--- Front-End-Project/Backend/express/index.d.ts
+++
@@ -1,6 +0,0 @@
-import { Request } from 'express';
-import { JwtPayload } from 'jsonwebtoken';
-
-export interface CustomRequest extends Request {
-  userData?: JwtPayload & { _id: string; userId?: string; email: string; displayName: string };
-}
